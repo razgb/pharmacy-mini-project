11dc8e6d05c1359ef97d6a3d70cf176c1ac9b5b5
razgb <hamaraz2003@gmail.com>
2023-10-29 21:57:15 +0300

    Lazy load version 1.0

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use script";
 
+//
+// NAVIGATION FADE OUT AND FADE IN EFFECT
+//
+
 const navContainer = document.querySelector(".nav__container");
-// const navLink = document.addEventListener(".nav__link");
 
 navContainer.addEventListener("mouseover", function (e) {
   e.preventDefault(); // incase button link is pressed.
@@ -16,11 +19,37 @@ navContainer.addEventListener("mouseover", function (e) {
   // 2) Remove blurry class from targeted element.
   navItem.classList.remove("lighten-effect");
 
-  console.log(navItem);
+  // console.log(navItem);
 });
 
 navContainer.addEventListener("mouseout", function (e) {
   e.preventDefault(); // incase button link is pressed.
   const allNavItems = document.querySelectorAll(".nav__item");
   allNavItems.forEach((item) => item.classList.remove("lighten-effect"));
+});
+
+//
+// INTERSECTION OBSERVER TO REMOVE (VIEW-OUT) CLASS FOR SECTIONS.
+//
+
+// Element and options setup.
+const sectionViewOut = document.querySelectorAll(".view-out");
+const sectionOptions = {
+  root: null, // means viewport by default
+  rootMargin: "", // how much above or below 50% the obsrv shows.
+  threshold: 0.2, // this means when 50% of the section shows.
+};
+
+// Instance of observer.
+const sectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("view-out");
+      console.log("Observer 🔭");
+    }
+  });
+}, sectionOptions);
+
+sectionViewOut.forEach((sect) => {
+  sectionObserver.observe(sect);
 });
