ffd50d95dff6acb4591aa1704372c3c26427db2b
razgb <hamaraz2003@gmail.com>
2023-10-31 13:52:28 +0300

    Simple restructure of JS file

--- script.js
+++ script.js
@@ -3,126 +3,136 @@
 //
 // NAVIGATION FADE OUT AND FADE IN EFFECT.
 //
+const navFadeAndScroll = function () {
+  const navContainer = document.querySelector(".nav__container");
 
-const navContainer = document.querySelector(".nav__container");
+  navContainer.addEventListener("mouseover", function (e) {
+    e.preventDefault(); // incase button link is pressed.
 
-navContainer.addEventListener("mouseover", function (e) {
-  e.preventDefault(); // incase button link is pressed.
+    const allNavItems = document.querySelectorAll(".nav__item");
+    const navItem = e.target.closest(".nav__item");
+    if (!navItem) return; // guard
 
-  const allNavItems = document.querySelectorAll(".nav__item");
-  const navItem = e.target.closest(".nav__item");
-  if (!navItem) return; // guard
+    // 1) Add blurry class to all nav__item elements.
+    allNavItems.forEach((item) => item.classList.add("lighten-effect"));
 
-  // 1) Add blurry class to all nav__item elements.
-  allNavItems.forEach((item) => item.classList.add("lighten-effect"));
+    // 2) Remove blurry class from targeted element.
+    navItem.classList.remove("lighten-effect");
 
-  // 2) Remove blurry class from targeted element.
-  navItem.classList.remove("lighten-effect");
+    // console.log(navItem);
+  });
 
-  // console.log(navItem);
-});
+  navContainer.addEventListener("mouseout", function (e) {
+    e.preventDefault(); // incase button link is pressed.
+    const allNavItems = document.querySelectorAll(".nav__item");
+    allNavItems.forEach((item) => item.classList.remove("lighten-effect"));
+  });
 
-navContainer.addEventListener("mouseout", function (e) {
-  e.preventDefault(); // incase button link is pressed.
-  const allNavItems = document.querySelectorAll(".nav__item");
-  allNavItems.forEach((item) => item.classList.remove("lighten-effect"));
-});
+  const navMap = {};
+
+  document.querySelectorAll(".nav__item").forEach((tab) => {
+    const textContent = tab.textContent.trim();
+    if (textContent.split(" ").length === 1) {
+      const id = `#section__${textContent.toLowerCase()}`;
+      navMap[textContent] = id;
+    }
+  });
+  // console.log(navMap);
+
+  ///////////////////
+
+  navContainer.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const navItem = e.target.closest(".nav__item");
+    if (!navItem) return;
+
+    const textContent = navItem.textContent.trim();
+    const targetSection = navMap[textContent];
+
+    if (targetSection) {
+      document
+        .querySelector(targetSection)
+        .scrollIntoView({ behavior: "smooth" });
+    }
+  });
+};
 
 //
 // INTERSECTION OBSERVER TO REMOVE (VIEW-OUT) CLASS FOR SECTIONS.
 //
+const AddViewObserver = function () {
+  // initialising last scroll position. The roof of the webpage.
+  let lastScrollY = null;
+  // Short delay so the page sets up.
+  const initialiseLastScrollY = () => {
+    setTimeout(() => {
+      lastScrollY = window.scrollY;
+    }, 500);
+  };
+  initialiseLastScrollY();
 
-// initialising last scroll position. The roof of the webpage.
-let lastScrollY = null;
-// Short delay so the page sets up.
-const initialiseLastScrollY = () => {
-  setTimeout(() => {
-    lastScrollY = window.scrollY;
-  }, 500);
-};
-initialiseLastScrollY();
+  const sectionViewOut = document.querySelectorAll(".view-out");
+  const sectionOptions = {
+    root: null, // means viewport by default
+    rootMargin: "", // how much above or below 50% the obsrv shows.
+    threshold: 0.2, // this means when 50% of the section shows.
+  };
 
-const sectionViewOut = document.querySelectorAll(".view-out");
-const sectionOptions = {
-  root: null, // means viewport by default
-  rootMargin: "", // how much above or below 50% the obsrv shows.
-  threshold: 0.2, // this means when 50% of the section shows.
-};
-
-// If page doesn't reload at the beginning.
-if (window.scrollY !== 0) {
-  sectionViewOut.forEach((sect) => {
-    sect.classList.remove("view-out");
-  });
-} else {
-  // Instance of observer.
-  const sectionObserver = new IntersectionObserver((entries) => {
-    if (lastScrollY === null) return;
-
-    const currentScrollY = window.scrollY;
-
-    entries.forEach((entry) => {
-      if (entry.isIntersecting && currentScrollY > lastScrollY) {
-        entry.target.classList.remove("view-out");
-        console.log("Observer 🔭: Scrolled downwards");
-      }
+  // If page doesn't reload at the beginning.
+  if (window.scrollY !== 0) {
+    sectionViewOut.forEach((sect) => {
+      sect.classList.remove("view-out");
     });
+  } else {
+    // Instance of observer.
+    const sectionObserver = new IntersectionObserver((entries) => {
+      if (lastScrollY === null) return;
 
-    lastScrollY = currentScrollY; // updating. (analogy: reseting value of the roof)
-  }, sectionOptions);
+      const currentScrollY = window.scrollY;
 
-  sectionViewOut.forEach((sect) => {
-    sectionObserver.observe(sect);
-  });
-}
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && currentScrollY > lastScrollY) {
+          entry.target.classList.remove("view-out");
+          console.log("Observer 🔭: Scrolled downwards");
+        }
+      });
 
-//
-// SMOOTH SCROLLING TO ALL FEATURES.
-//
+      lastScrollY = currentScrollY; // updating. (analogy: reseting value of the roof)
+    }, sectionOptions);
 
-const navMap = {};
-
-document.querySelectorAll(".nav__item").forEach((tab) => {
-  const textContent = tab.textContent.trim();
-  if (textContent.split(" ").length === 1) {
-    const id = `#section__${textContent.toLowerCase()}`;
-    navMap[textContent] = id;
+    sectionViewOut.forEach((sect) => {
+      sectionObserver.observe(sect);
+    });
   }
-});
-// console.log(navMap);
-
-///////////////////
-
-navContainer.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  const navItem = e.target.closest(".nav__item");
-  if (!navItem) return;
-
-  const textContent = navItem.textContent.trim();
-  const targetSection = navMap[textContent];
-
-  if (targetSection) {
-    document
-      .querySelector(targetSection)
-      .scrollIntoView({ behavior: "smooth" });
-  }
-});
+};
 
 //
 // OPERATIONS BUTTONS ANIMATION EFFECT.
 //
+const addOperationEffects = function () {
+  document
+    .querySelector(".operations__tab--container")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      const clickedTab = e.target.closest(".operations__tab");
+      if (!clickedTab) return;
 
-document
-  .querySelector(".operations__tab--container")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-    const clickedTab = e.target.closest(".operations__tab");
-    if (!clickedTab) return;
-
-    document.querySelectorAll(".operations__tab").forEach((tab) => {
-      tab.classList.remove("operations__tab--active");
+      document.querySelectorAll(".operations__tab").forEach((tab) => {
+        tab.classList.remove("operations__tab--active");
+      });
+      clickedTab.classList.add("operations__tab--active");
+      return;
     });
-    clickedTab.classList.add("operations__tab--active");
-    return;
-  });
+
+  //
+  // OPERATIONS BOX LEFT/RIGHT CHANGE EFFECT:
+  //
+};
+
+const init = function () {
+  navFadeAndScroll();
+  AddViewObserver();
+  addOperationEffects();
+};
+init();
